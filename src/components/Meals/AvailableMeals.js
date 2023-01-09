a22fbd42efb117785474a46567b391489b8de7c2
Yuliia <julia.novak9@gmail@com>
2023-01-09 11:45:25 +0100

    fetch meals data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,38 @@
+import { useEffect, useState } from "react";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 
-const DUMMY_MEALS = [
-   {
-      id: "m1",
-      name: "Sushi",
-      description: "Finest fish and veggies",
-      price: 22.99,
-   },
-   {
-      id: "m2",
-      name: "Schnitzel",
-      description: "A german specialty!",
-      price: 16.5,
-   },
-   {
-      id: "m3",
-      name: "Barbecue Burger",
-      description: "American, raw, meaty",
-      price: 12.99,
-   },
-   {
-      id: "m4",
-      name: "Green Bowl",
-      description: "Healthy...and green...",
-      price: 18.99,
-   },
-];
-
 const AvailableMeals = () => {
-   const mealsList = DUMMY_MEALS.map((meal) => (
+   const [meals, setMeals] = useState([]);
+   const [error, setError] = useState();
+
+   const fetchMealsHandler = async () => {
+      try {
+         const response = await fetch(
+            "https://food-order-1cdae-default-rtdb.firebaseio.com/meals.json"
+         );
+         const data = await response.json();
+         const loadedMeals = [];
+         for (const key in data) {
+            loadedMeals.push({
+               id: key,
+               name: data[key].name,
+               description: data[key].description,
+               price: data[key].price,
+            });
+         }
+         setMeals(loadedMeals);
+      } catch (error) {
+         return setError(error);
+      }
+   };
+
+   useEffect(() => {
+      fetchMealsHandler();
+   }, [meals]);
+
+   const mealsList = meals.map((meal) => (
       <MealItem
          id={meal.id}
          key={meal.id}
@@ -41,11 +43,17 @@ const AvailableMeals = () => {
    ));
 
    return (
-      <section className={classes.meals}>
-         <Card>
-            <ul>{mealsList}</ul>
-         </Card>
-      </section>
+      <>
+         <section className={classes.meals}>
+            {error ? (
+               <p>AN ERROR OCCURED!</p>
+            ) : (
+               <Card>
+                  <ul>{mealsList}</ul>
+               </Card>
+            )}
+         </section>
+      </>
    );
 };
 
